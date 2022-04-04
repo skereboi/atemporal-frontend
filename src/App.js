5bb2c90e6aba3dd9769dd5bc60355c82390d612f
Daniel Cu Sánchez <danielcu@alternet.com.mx>
2022-04-04 13:15:09 -0500

    use context auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { RequireAuth } from './components/Auth/RequireAuth'
+import { DashboardHomePage } from './pages/Dashboard/Home'
+import { Layout } from './pages/Layout'
+import { LoginPage } from './pages/Login'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Layout>
+        <h1>Router</h1>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/dashboard"
+              element={
+                <RequireAuth>
+                  <DashboardHomePage />
+                </RequireAuth>
+              }
+            />
+          </Route>
+        </Routes>
+      </Layout>
+    </>
+  )
 }
 
-export default App;
+export default App
