79c46e6d53d14bbc2b66ba1d0c42399acfbfe6fc
Saulrs98 <A01351663@itesm.mx>
2022-05-03 23:16:44 -0500

    Dropdown perfil

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../hooks/useAuth'
+import './style.scss'
 
 export const Profile = () => {
   const { user } = useAuth()
