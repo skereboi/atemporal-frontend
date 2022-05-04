873aed82bcec41bea89e5fd9592efe3020823e69
Daniel Cu Sánchez <danielcu@alternet.com.mx>
2022-05-04 09:26:17 -0500

    configuración para deploy en netlify

--- src/services/event.service.js
+++ src/services/event.service.js
@@ -1,13 +1,14 @@
 /* eslint-disable camelcase */
 import Axios from 'axios'
+import globalConfig from '../config'
 
 export const eventService = {
   getAllEvents: async () => {
-    const { data } = await Axios.get('http://localhost:4000/eventos')
+    const { data } = await Axios.get(`${globalConfig.url}/eventos`)
     return data
   },
   getOneEvent: async (id) => {
-    const { data } = await Axios.get(`http://localhost:4000/eventos/${id}`)
+    const { data } = await Axios.get(`${globalConfig.url}/eventos/${id}`)
     return data
   }
 }
