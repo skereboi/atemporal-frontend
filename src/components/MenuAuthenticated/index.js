eecd2facfdcce0e605bc36d7c7333ae5960ec238
Daniel Cu Sánchez <danielcu@alternet.com.mx>
2022-05-07 10:09:34 -0500

    menu auth

--- src/components/MenuAuthenticated/index.js
+++ src/components/MenuAuthenticated/index.js
@@ -2,26 +2,36 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../hooks/useAuth'
 import { Profile } from '../Profile'
+import './style.scss'
 
 export const MenuAuthenticated = () => {
-  const { user } = useAuth()
   return (
     <>
-      <div>
-        <Link to={user.typeUser === 'admin' ? 'dashboard/aprobar' : '/eventos'} className='text-light p-4'>
-          {
-            user.typeUser === 'general' ? 'Eventos' : 'Aprobar eventos'
-          }
-        </Link>
-        {
-          user.typeUser === 'general' && (<Link to='/crear-evento' className='text-light p-4'>Crear evento</Link>)
-        }
-        {
-          user.typeUser === 'admin' && (<Link to='dashboard/eventos' className='text-light p-4'>Eventos aprobados</Link>)
-        }
+      <div className='menu-authenticated'>
+        <div className="menu-web">
+          <OptionsMenuAuth/>
+        </div>
 
         <Profile/>
       </div>
+    </>
+  )
+}
+export const OptionsMenuAuth = () => {
+  const { user } = useAuth()
+  return (
+    <>
+      <Link to={user.typeUser === 'admin' ? 'dashboard/aprobar' : '/eventos'} className='text-light p-4'>
+        {
+          user.typeUser === 'general' ? 'Eventos' : 'Aprobar eventos'
+        }
+      </Link>
+      {
+        user.typeUser === 'general' && (<Link to='/crear-evento' className='text-light p-4'>Crear evento</Link>)
+      }
+      {
+        user.typeUser === 'admin' && (<Link to='dashboard/eventos' className='text-light p-4'>Eventos aprobados</Link>)
+      }
     </>
   )
 }
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../hooks/useAuth'
+import { OptionsMenuAuth } from '../MenuAuthenticated'
 import './style.scss'
 
 export const Profile = () => {
-  const { user } = useAuth()
+  const { user, logout } = useAuth()
   return (
     <>
-
-      <Link to={user.typeUser === 'admin' ? 'dashboard/mi-cuenta' : '/mi-cuenta'} className='btn btn-primary btn-sm fs-sm rounded d-none d-lg-inline-flex'>
-        <i className="bx bx-user fs-5 lh-1 me-1" />
-        {user.nombre}
-      </Link>
+      <div className="btn-group dropdown profile-btn">
+        <button type="button" className='btn btn-primary btn-sm fs-sm rounded d-none d-lg-inline-flex  dropdown-toggle' data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i className="bx bx-user fs-5 lh-1 me-1" />
+          {user.nombre}
+        </button>
+        <div className="dropdown-menu my-1">
+          <Link to={user.typeUser === 'admin' ? 'dashboard/mi-cuenta' : '/mi-cuenta'} className="dropdown-item">
+            Mi cuenta
+          </Link>
+          <div className="menu-mobile">
+            <OptionsMenuAuth />
+          </div>
+          <div className="dropdown-divider" />
+          <button onClick={() => logout()} className="dropdown-item">Cerrar sesión</button>
+        </div>
+      </div>
 
     </>
   )
