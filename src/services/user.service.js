0a2731896d72acf813998373e823965e3c4b13da
Daniel Cu Sánchez <danielcu@alternet.com.mx>
2022-05-16 19:29:21 -0500

    Cambiar datos de cuenta de usuario

---
+++ src/services/user.service.js
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+import Axios from 'axios'
+import globalConfig from '../config'
+
+export const userService = {
+  getOneUser: async () => {
+    const { data } = await Axios.get(`${globalConfig.url}/api/usuarios/me`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('TOKEN_ATEMPORAL')}`
+      }
+    })
+    return data
+  },
+  updateOneUser: async (id, user) => {
+    console.log('ID_USER', id)
+    const { data } = await Axios.put(`${globalConfig.url}/api/usuarios/${id}`, user)
+    return data
+  }
+}
