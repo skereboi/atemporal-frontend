84902d9400ba293e03da1a5d3ddd0e0e8ad1a334
Daniel Cu Sánchez <danielcu@alternet.com.mx>
2022-05-26 12:53:31 -0500

    arreglo de dashboard

--- src/pages/Dashboard/Home/index.js
+++ src/pages/Dashboard/Home/index.js
@@ -1,23 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { eventService } from '../../../services/event.service'
 
 export const DashboardHomePage = () => {
+  const [totalAproved, setTotalAproved] = useState([])
+  const [totalToAproved, setTotalToAproved] = useState([])
+  const [totalDeleted, setTotalDeleted] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const totalA = await eventService.getAllEventsAproved()
+        setTotalAproved(totalA)
+        const totalTo = await eventService.getAllEventsToAproved()
+        setTotalToAproved(totalTo)
+        const totalD = await eventService.getAllEventsDeleted()
+        setTotalDeleted(totalD)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+  }, [])
+
   return (
     <div className="row mt-5">
       <div className="col-md-4">
         <div className="card text-white bg-primary">
-          <div className="card-header border-light">Eventos registrados</div>
+          <div className="card-header border-light">Eventos pendientes por aprobar</div>
           <div className="card-body">
-            <h5 className="card-title text-white">Primary card title</h5>
-            <p className="card-text fs-sm">Some quick example text to build on the card title and make up the bulk of the content.</p>
+            <h5 className="card-title text-white">
+              {
+                totalToAproved.length
+              }
+            </h5>
+            <p className="card-text fs-sm">Total de eventos pendientes</p>
           </div>
         </div>
       </div>
       <div className="col-md-4">
         <div className="card text-white bg-success">
-          <div className="card-header border-light">Eventos aprobados</div>
+          <div className="card-header border-light">Eventos publicados</div>
           <div className="card-body">
-            <h5 className="card-title text-white">Success card title</h5>
-            <p className="card-text fs-sm">Some quick example text to build on the card title and make up the bulk of th content.</p>
+            <h5 className="card-title text-white">
+              {
+                totalAproved.length
+              }
+            </h5>
+            <p className="card-text fs-sm">
+              Todos los eventos aprobados
+            </p>
           </div>
         </div>
       </div>
@@ -25,8 +56,12 @@ export const DashboardHomePage = () => {
         <div className="card text-white bg-danger">
           <div className="card-header border-light">Eventos rechazados</div>
           <div className="card-body">
-            <h5 className="card-title text-white">Danger card title</h5>
-            <p className="card-text fs-sm">Some quick example text to build on the card title and make up the bulk of the content.</p>
+            <h5 className="card-title text-white">
+              {
+                totalDeleted.length
+              }
+            </h5>
+            <p className="card-text fs-sm">Todos los elementos eliminados</p>
           </div>
         </div>
 
